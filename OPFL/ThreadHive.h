c4a446cb1a132af7fc3cec67b49676ad84750f1a
Aaron Goschie <43655193+agoschie@users.noreply.github.com>
2025-09-04 21:49:11 -0700

    Added "please read" comment

--- OPFL/ThreadHive.h
+++ OPFL/ThreadHive.h
@@ -1,3 +1,4 @@
+// PLEASE READ: OPFL was written when I was still teaching myself C++ with no formal education. As a result, the code could have been written much better in hindsight.
 /*
  * ThreadHive.h
  *
